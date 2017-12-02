2dee81ed055bedbe914f518631df16e4df563a6c
Guilherme Pompilio <guilherme.pomp@gmail.com>
2017-12-02 10:12:36 -0200

    Codigo Concluido

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LeitorArquivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMostraTextos = new System.Windows.Forms.TextBox();
+            this.txtGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMostraTextos
+            // 
+            this.txtMostraTextos.Location = new System.Drawing.Point(12, 12);
+            this.txtMostraTextos.Multiline = true;
+            this.txtMostraTextos.Name = "txtMostraTextos";
+            this.txtMostraTextos.Size = new System.Drawing.Size(260, 108);
+            this.txtMostraTextos.TabIndex = 0;
+            // 
+            // txtGravar
+            // 
+            this.txtGravar.Location = new System.Drawing.Point(95, 189);
+            this.txtGravar.Name = "txtGravar";
+            this.txtGravar.Size = new System.Drawing.Size(75, 23);
+            this.txtGravar.TabIndex = 1;
+            this.txtGravar.Text = "Gravar";
+            this.txtGravar.UseVisualStyleBackColor = true;
+            this.txtGravar.Click += new System.EventHandler(this.txtGravar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtGravar);
+            this.Controls.Add(this.txtMostraTextos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMostraTextos;
+        private System.Windows.Forms.Button txtGravar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeitorArquivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("texto.txt"))
+            {
+                Stream entrada = File.Open("texto.txt", FileMode.Open);
+                StreamReader leitor = new StreamReader(entrada);
+                string linha = leitor.ReadToEnd();
+                leitor.Close();
+                entrada.Close();
+            }
+        }
+
+        private void txtGravar_Click(object sender, EventArgs e)
+        {
+            Stream saida = File.Open("texto.txt", FileMode.Create);
+            StreamWriter escritor = new StreamWriter(saida);
+            escritor.Write(txtMostraTextos.Text);
+            escritor.Close();
+            saida.Close();
+        }
+    }
+}
